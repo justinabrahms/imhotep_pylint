e7daf90f82bbeac94029ad0da7d7caa032e6aec8
Justin Abrahms <justin@abrah.ms>
2014-01-18 16:28:11 -0800

    Initial Import

---
+++ imhotep_pylint/plugin.py
@@ -0,0 +1,32 @@
+from imhotep.tools import Tool
+from collections import defaultdict
+import re
+
+class PyLint(Tool):
+    response_format = re.compile(r'(?P<filename>.*):(?P<line_num>\d+):'
+                                 '(?P<message>.*)')
+    pylintrc_filename = '.pylintrc'
+
+    def get_file_extensions(self):
+        return ['.py']
+
+    def process_line(self, dirname, line):
+        match = self.response_format.search(line)
+        if match is not None:
+            if len(self.filenames) != 0:
+                if match.group('filename') not in self.filenames:
+                    return
+            filename, line, messages = match.groups()
+            # If you run pylint on /foo/bar/baz and you are in the /foo/bar
+            # directory, it will spit out paths that look like: ./baz To fix
+            # this, we run it through `os.path.abspath` which will give it a
+            # full, absolute path.
+            filename = os.path.abspath(filename)
+            return filename, line, messages
+
+    def get_command(self, dirname):
+        cmd = 'pylint --output-format=parseable -rn'
+        if os.path.exists(os.path.join(dirname, self.pylintrc_filename)):
+            cmd += " --rcfile=%s" % os.path.join(
+                dirname, self.pylintrc_filename)
+        return cmd
---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='imhotep_pylint',
+    version='0.0.1',
+    packages=find_packages(),
+    url='https://github.com/justinabrahms/imhotep_pylint',
+    license='MIT',
+    author='Justin Abrahms',
+    author_email='justin@abrah.ms',
+    description='An imhotep plugin for pylint validation',
+    requires=['pylint'],
+    entry_points={
+        'imhotep_linters': [
+            '.py = imhotep_pylint.plugin:PyLint'
+        ],
+    },
+)
